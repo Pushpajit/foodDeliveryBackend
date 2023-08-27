15e0db616bf72d361873fb43a7f4dd3e7bf290fa
Pushpajit Biswas <pushpajitnexus007@gmail.com>
2023-08-27 14:53:04 +0530

    cors fix

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express=require("express")
 const app=express()
 const port=5000
 const mongoDB=require("./db");
+const cors = require('cors');
 mongoDB();
 
 // ye to likhna he padega pta nai kyu  bs likhna hai fix hai likhna
-app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        res.header(
-          "Access-Control-Allow-Headers",
-          "Origin, X-Requested-With, Content-Type, Accept"
-        );
-        next();
-      });
+// app.use((req, res, next) => {
+//         res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//         res.header(
+//           "Access-Control-Allow-Headers",
+//           "Origin, X-Requested-With, Content-Type, Accept"
+//         );
+//         next();
+//       });
 
+app.use(cors());
 //----------------------------------------------------------------------------
 
 app.get('/' ,(req,res)=>{
